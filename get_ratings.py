038b8f391fe2c6a563355a2dbd5b1f4b9a26140f
Kevin Xu <84105756+K3V1N321@users.noreply.github.com>
2025-05-26 01:39:13 -0700

    ratings

--- get_ratings.py
+++ get_ratings.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import time
+from tqdm import tqdm
 
 API_KEY = "AIzaSyB6U9fR1Uk1OGK5NTra1oClMvIcaoQpPp4"
     
@@ -26,34 +28,15 @@ def get_rating(place_id):
     url = "https://maps.googleapis.com/maps/api/place/details/json"
     params = {
         'place_id': place_id,
-        'fields': 'rating',
+        'fields': 'rating,reviews',
         'key': API_KEY
     }
     response = requests.get(url, params=params)
     data = response.json()
-    return data.get('result', {}).get('rating')
-
-def place_id_main():
-    df = pd.read_csv("ratings_data.csv")
-    new = df.copy()
-    indices = df.loc[df["id"] == "Unknown"].index
-    for i in indices:
-        print(i)
-        if i == 20500:
-            break
-        place = new.loc[i, "query"]
-        id = new.loc[i, "id"]
-        if id != "Unknown":
-            continue
-        pid = get_place_id(place)
-        if pid:
-           print(f"Place ID for '{place}': {pid}")
-           new.loc[i, "id"] = pid
-        else:
-            print(f"No Place ID found for '{place}'")
-            new.loc[i, "id"] = "fail"
-        new.to_csv("ratings_data.csv", index = False)
-
+    rating = data.get('result', {}).get('rating')
+    reviews = data.get('result', {}).get('reviews', [])[:5]
+    return rating, reviews
+    
 def rating_main():
     df = pd.read_csv("ratings_data.csv")
     indices = df.loc[df["rating"] == "Unknown"].index
@@ -71,6 +54,7 @@ def rating_main():
             new.loc[i, "rating"] = "fail"
             continue
         rating = get_rating(id)
+        time.sleep(1)
         print(f"Rating for '{placeName}': {rating}")
         if rating == None:
             new.loc[i, "rating"] = "fail"
@@ -79,7 +63,40 @@ def rating_main():
         new.to_csv("ratings_data.csv", index = False)
     
 if __name__ == "__main__":
-    # Get ids of locations
-    place_id_main()
-    # Get ratings of locations
-    # rating_main()
+    df = pd.read_csv("ratings_data.csv")
+    new = df.copy()
+    indices = df.loc[df["id"] == "Unknown"].index
+    indicesUse = indices[0:1000]
+    reviewsSave = []
+    for i in tqdm(indicesUse):
+        place = new.loc[i, "query"]
+        id = new.loc[i, "id"]
+        if id != "Unknown":
+            continue
+        id = get_place_id(place)
+        time.sleep(1)
+        if id:
+           print(f"Place ID for '{place}': {id}")
+           new.loc[i, "id"] = id
+           rating, reviews = get_rating(id)
+           new.loc[i, "rating"] = rating
+           for j in range(len(reviews)):
+               reviews[j]["query"] = place
+           
+           reviewsSave.append(reviews)
+           print(f"Rating for '{place}': {rating}")
+           time.sleep(1)
+        else:
+            print(f"No Place ID found for '{place}'")
+            new.loc[i, "id"] = "fail"
+            new.loc[i, "rating"] = "fail"
+            reviewsSave.append("fail")
+        new.to_csv("ratings_data2.csv", index = False)
+        
+    reviewsDf = pd.read_csv("reviews.csv")
+    for review in reviewsSave:
+        if type(review) != list:
+            continue
+        a = pd.DataFrame(review)
+        reviewsDf = pd.concat([reviewsDf, a], axis = 0)
+    reviewsDf.to_csv("reviews.csv", index = False)
